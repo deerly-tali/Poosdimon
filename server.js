55b0b72ba19d8ce52f1bc28f12480e82baac3010
Talia Martin <67079575+deerly-tali@users.noreply.github.com>
2023-11-06 14:22:53 -0500

    pokeApi endpoint is fixed!

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express"); //espress for backend
-const cors = require("cors");
+//const cors = require("cors");
 const axios = require('axios');
 
 const app = express();
@@ -8,8 +8,8 @@ const admin = require('firebase-admin'); //we will use firebase auth
 const serviceAccount = require("./serviceKey.json"); //this is vital for firebase auth && PLS gitignore this
 
 
-app.use(cors);
-app.use(express.json()); //we will be handling json objs
+//app.use(cors);
+//app.use(express.json()); //we will be handling json objs
 app.use(express.urlencoded({extended: true})); //we will parse to our url
 
 
@@ -19,7 +19,7 @@ admin.initializeApp({
 });
 
 
-
+//to debug
 app.get('/hello', function(req,res){
     res.send("hello worlds");
 });
@@ -29,15 +29,23 @@ app.get('/hello', function(req,res){
 app.get('/pokemon/:name', async (req,res) =>{ //TODO: the request grabs from api but returns null & server fails
     try{
         const {name} = req.params;
-        const response = axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-        const pokemon = response.data;
-
-        res.json({pokemon}.name);//this is returning null
+        const response = axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((pokemon) => {
+            console.log(pokemon.data);
+            const data = pokemon.data;
+            res.send(data);
+        })
+        .catch(function (error){
+            console.log(error);
+        });
 
     }catch(error){
         res.status(404).send(error.message);
     }
 });
+
+
+//TODO: get pokemon from showdown
 
 
 
