b4ce2bf5d8e825195a1252722bd3d7535a5ce225
Carlos Martinez <carlose082002@gmail.com>
2023-11-12 20:47:01 -0500

    rewriting front end

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const serviceAccount = require("./serviceKey.json"); //this is vital for firebas
 //app.use(cors);
 //app.use(express.json()); //we will be handling json objs
 app.use(express.urlencoded({extended: true})); //we will parse to our url
-
+app.use(express.static('public'));
 
 
 admin.initializeApp({
