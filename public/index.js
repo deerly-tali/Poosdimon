4f36fcd9bc34ceae0aa7b8838142b01d58acfe63
Talia Martin <67079575+deerly-tali@users.noreply.github.com>
2023-11-21 15:19:41 -0500

    updating security on firebase config

--- public/index.js
+++ public/index.js
@@ -1,26 +1,16 @@
-alert("JS working");
+alert("JS working"); //TODO: delete, this is just for debug purposes
+//importing the functions we will use
 const { intializeApp } = require("firebase/app");
 const { getAuth,
         signInWithEmailAndPassword,
         onAuthStateChanged,
         signOut
     } = require("firebase/auth");
-/*
-// Import the functions we will use
+
 
 // our Firebase setup
-const firebaseConfig = {
-  apiKey: "AIzaSyAWNsQGx_Iw_y9e0N5zPqndiv6kk64SRiE",
-  authDomain: "fir-demotest-105b0.firebaseapp.com",
-  databaseURL: "https://fir-demotest-105b0-default-rtdb.firebaseio.com",
-  projectId: "fir-demotest-105b0",
-  storageBucket: "fir-demotest-105b0.appspot.com",
-  messagingSenderId: "505860360257",
-  appId: "1:505860360257:web:91b4277854be27e8501ec0",
-  measurementId: "G-8GC3VQ6N0S"
-
-};
-
+//const firebaseConfig = require('firebaseConfig.json');
+/*
 // Initialize Firebase 
 const app = initializeApp(firebaseConfig);//setting up our app
 const auth = getAuth(app);//connecting our auth to our app
