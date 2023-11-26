0032153981ee19f60af6da6e86da392c0ace9739
Talia Martin <67079575+deerly-tali@users.noreply.github.com>
2023-11-25 18:35:13 -0600

    update onChildRemoved

--- public/app.js
+++ public/app.js
@@ -293,12 +293,18 @@ const mapData = {
   */
   
       //Remove character DOM element after they leave
+      onChildRemoved(allPlayersRef, (snapshot) => {
+        const removedKey = snapshot.val().id;
+        gameContainer.removeChild(playerElements[removedKey]);
+        delete playerElements[removedKey];
+      });
+      /*
       allPlayersRef.on("child_removed", (snapshot) => {
         const removedKey = snapshot.val().id;
         gameContainer.removeChild(playerElements[removedKey]);
         delete playerElements[removedKey];
       })
-  
+  */
   
       //New - not in the video!
       //This block will remove coins from local state when Firebase `coins` value updates
